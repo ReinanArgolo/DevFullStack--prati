3abcf961aa3d5176fbb89134b577bdd887bab99c
Reinan Argolo <reinanlopes.1@gmail.com>
2024-07-03 19:28:39 -0300

    Início Módulo 04

--- Modulo4/lesson01/VanillaTabs/app.js
+++ Modulo4/lesson01/VanillaTabs/app.js
@@ -19,42 +19,41 @@ const conteudos = [
 const btnTab1 = document.getElementById('btn-tab1')
 const btnTab2 = document.getElementById('btn-tab2')
 const btnTab3 = document.getElementById('btn-tab3')
-
 const content = document.getElementById('content')
 
 
-function dispalyContent(items) {
+function displayContent(items) {
     let listaContent = ''
-    for(const item of items) {
+    for(const item of items){
         listaContent += `<li>${item}</li>`
     }
 
     const lista = document.createElement('ul')
-    lista.innerHTML = listaContent
-    content.appendChild(lista)
-}
-
-function handleClick(event) {
-    const btnId = event.target.id
-    activateButton(event.target)
-
-    if(btnId === 'btn-tab1') {
-        dispalyContent(conteudos[0])
-    } else if (btnId === 'btn-tab2') {
-        dispalyContent(conteudos[1])
-    } else {
-        dispalyContent(conteudos[2])
-    }
+    content.innerHTML = listaContent
+    content.append(lista)
 }
 function activateButton(btn) {
     btnTab1.className = ""
     btnTab2.className = ""
     btnTab3.className = ""
-    btn.className='active'
+    btn.className = "active"
+}
+function handleClick(event) {
+    const btnId = event.target.id
+    activateButton(event.target)
+
+    if(btnId === "btn-tab1"){
+        displayContent(conteudos[0])
+    } else if (btnId === "btn-tab2") {
+        displayContent(conteudos[1])
+    } else {
+        displayContent(conteudos[2])
+    }
 }
 
-btnTab1.addEventListener('click', handleClick)
-btnTab2.addEventListener('click', handleClick)
-btnTab3.addEventListener('click', handleClick)
 
-dispalyContent(conteudos[0])
+btnTab1.addEventListener("click", handleClick)
+btnTab2.addEventListener("click", handleClick)
+btnTab3.addEventListener("click", handleClick)
+
+displayContent(conteudos[0])
